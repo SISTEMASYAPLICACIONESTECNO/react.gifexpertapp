ccb9fd5a3bbb623952e5c2a04de6a985bf524e51
JOSE ANTONIO GONZALEZ PALOMO <sistemasyaplicacionestecno@gmail.com>
2020-10-24 17:35:37 -0500

    docs agregado

---
+++ docs/precache-manifest.5139bab3ae1556c84aee5951a442a097.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b4175103d768dbcc56289824e53e187",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bc49b0d1a2a5361d636",
+    "url": "/static/css/main.9f0cb6cb.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc49b0d1a2a5361d636",
+    "url": "/static/js/main.17b4d11b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
